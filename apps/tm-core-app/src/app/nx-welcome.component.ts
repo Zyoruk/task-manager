27c715c21f77f48f4108462c5788a45ca3939ba3
Luis Simon <lsimon@godaddy.com>
2024-08-30 21:44:53 -0600

    dont  SSR core app

--- apps/tm-core-app/src/app/nx-welcome.component.ts
+++ apps/tm-core-app/src/app/nx-welcome.component.ts
@@ -742,11 +742,7 @@ import { CommonModule } from '@angular/common';
               </div>
               <p>Your Nx Cloud remote cache setup is almost complete.</p>
 
-              <a
-                href="https://cloud.nx.app/connect/X66QSbL6zY"
-                target="_blank"
-                rel="noreferrer"
-              >
+              <a href="" target="_blank" rel="noreferrer">
                 Click here to finish
               </a>
             </div>
