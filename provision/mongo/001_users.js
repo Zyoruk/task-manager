cd0bad2f2bb05de5b10c1f63c634ada8546100aa
Luis Simon <lsimon@godaddy.com>
2024-09-02 20:26:58 -0600

    setup mongo and mq

---
+++ provision/mongo/001_users.js
@@ -0,0 +1,12 @@
+db.createUser(
+    {
+        user: "newuser",
+        pwd: "newpassword",
+        roles:[
+            {
+                role: "readWrite",
+                db:   "taskmanagerdb"
+            }
+        ]
+    }
+);
