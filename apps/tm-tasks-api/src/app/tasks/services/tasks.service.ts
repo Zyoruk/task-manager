3330db5a4220d3bbd2fcb9ca79d25c846ebc2bc9
Luis Simon <lsimon@godaddy.com>
2024-09-09 23:31:46 -0600

    Tasks API: Improve Error handling

--- apps/tm-tasks-api/src/app/tasks/services/tasks.service.ts
+++ apps/tm-tasks-api/src/app/tasks/services/tasks.service.ts
@@ -25,12 +25,6 @@ export class TasksService {
         action: 'create',
         payload: newTask,
       });
-      //   this.notificationsServiceClient.send(
-      //     {
-      //       cmd: 'update',
-      //     },
-      //     newTask
-      //   );
       return newTask;
     } catch (error) {
       Logger.error('Failed to create task');
@@ -39,38 +33,72 @@ export class TasksService {
     }
   }
 
-  getAll(user: IUser) {
-    return this.taskModel
-      .find({
-        $or: [{ reportedBy: user._id }, { assignedTo: user._id }],
-      })
-      .exec();
+  async getAll(user: IUser) {
+    try {
+      const allTasks = await this.taskModel
+        .find({
+          $or: [{ reportedBy: user._id }, { assignedTo: user._id }],
+        })
+        .exec();
+      return allTasks;
+    } catch (error) {
+      Logger.error('Failed to get all tasks');
+      Logger.debug(error);
+      throw new Error(error);
+    }
   }
 
-  deleteTask(taskId: string, userContext: IUser) {
-    return this.taskModel
-      .deleteOne({
-        taskId,
-        $or: [{ reportedBy: userContext._id }, { assignedTo: userContext._id }],
-      })
-      .exec();
+  async deleteTask(taskId: string, userContext: IUser) {
+    try {
+      const deletedResult = await this.taskModel
+        .deleteOne({
+          taskId,
+          $or: [
+            { reportedBy: userContext._id },
+            { assignedTo: userContext._id },
+          ],
+        })
+        .exec();
+      return deletedResult;
+    } catch (error) {
+      Logger.error('Failed to delete task');
+      Logger.debug(error);
+      throw new Error(error);
+    }
   }
 
   getTask(taskId: string, userContext: IUser) {
-    return this.taskModel.findOne({
-      taskId,
-      $or: [{ reportedBy: userContext._id }, { assignedTo: userContext._id }],
-    });
+    try {
+      const task = this.taskModel.findOne({
+        taskId,
+        $or: [{ reportedBy: userContext._id }, { assignedTo: userContext._id }],
+      });
+      return task;
+    } catch (error) {
+      Logger.error('Failed to get task');
+      Logger.debug(error);
+      throw new Error(error);
+    }
   }
 
   updateTask(taskId: string, task: UpdateTaskDTO, userContext: IUser) {
-    return this.taskModel.findOneAndUpdate(
-      {
-        taskId: taskId,
-        $or: [{ reportedBy: userContext._id }, { assignedTo: userContext._id }],
-      },
-      task,
-      { new: true }
-    );
+    try {
+      const updatedTask = this.taskModel.findOneAndUpdate(
+        {
+          taskId: taskId,
+          $or: [
+            { reportedBy: userContext._id },
+            { assignedTo: userContext._id },
+          ],
+        },
+        task,
+        { new: true }
+      );
+      return updatedTask;
+    } catch (error) {
+      Logger.error('Failed to update task');
+      Logger.debug(error);
+      throw new Error(error);
+    }
   }
 }
