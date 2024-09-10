0ab02b6b4f71f577bdd469606647752f1ab0afe4
Luis Simon <lsimon@godaddy.com>
2024-09-09 21:05:37 -0600

    integrate auth in getter for POC

--- apps/tm-tasks-api/src/app/app.controller.ts
+++ apps/tm-tasks-api/src/app/app.controller.ts
@@ -1,8 +1,19 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 
 import { AppService } from './app.service';
+import { JwtAuthGuard } from './auth/guards/jwt.guard';
+import { ApiBearerAuth } from '@nestjs/swagger';
+import { UserContextInterceptor } from './auth/interceptors/user-context.interceptor';
+import { UserContext } from './auth/decorators/user-context.decorator';
 
 @Controller()
+@UseInterceptors(UserContextInterceptor)
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
@@ -12,7 +23,9 @@ export class AppController {
   }
 
   @Get()
-  getData() {
+  @UseGuards(JwtAuthGuard)
+  @ApiBearerAuth()
+  async getData(@UserContext() userContext: any) {
     return this.appService.getData();
   }
 }
