20c57e0ed33b47e53c4298cfcb41834c5ccad82d
Luis Simon <lsimon@godaddy.com>
2024-09-09 21:05:58 -0600

    move notifications calls elsewhere for now

--- apps/tm-tasks-api/src/app/app.service.ts
+++ apps/tm-tasks-api/src/app/app.service.ts
@@ -1,31 +1,32 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, Mongoose } from 'mongoose';
-import { Task, TaskSchema } from './schemas/task';
+import { Model } from 'mongoose';
+import { Task } from './schemas/task';
 
 @Injectable()
 export class AppService {
-  constructor(@Inject('NOTIFICATIONS_SERVICE') public notificationsServiceClient: ClientProxy, @InjectModel(Task.name) private taskModel: Model<Task>) { }
+  constructor(
+    @Inject('NOTIFICATIONS_SERVICE')
+    public notificationsServiceClient: ClientProxy,
+    @InjectModel(Task.name) private taskModel: Model<Task>
+  ) {}
 
   createNewTask() {
     this.notificationsServiceClient.emit('task', {
-      taskId: '123'
-    })
+      taskId: '123',
+    });
+    this.notificationsServiceClient.send(
+      {
+        cmd: 'update',
+      },
+      {
+        taskId: '123',
+      }
+    );
   }
 
   getData() {
-    this.notificationsServiceClient.emit('task', {
-      taskId: '123'
-    })
-    return this.notificationsServiceClient.send(
-      {
-        cmd: "update",
-      },
-      {
-        taskId: '123'
-      }
-    )
     return this.taskModel.find().exec();
   }
 }
