dc675333db71eba4035d9c12b37abd25ec8619ed
Luis Simon <lsimon@godaddy.com>
2024-09-12 15:58:21 -0600

    Tasks API clean up app controller

--- apps/tm-tasks-api/src/app/app.controller.ts
+++ apps/tm-tasks-api/src/app/app.controller.ts
@@ -1,26 +1,9 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Res,
-  UseGuards,
-  UseInterceptors,
-} from '@nestjs/common';
-
+import { Controller, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
-import { JwtAuthGuard } from './auth/guards/jwt.guard';
-import { ApiBearerAuth, ApiBody, ApiExtraModels } from '@nestjs/swagger';
 import { UserContextInterceptor } from './auth/interceptors/user-context.interceptor';
-import { UserContext } from './auth/decorators/user-context.decorator';
-import { IUser } from './types/user';
-import { CreateTaskDTO } from './dto/create-task.dto';
-import { TaskStatus } from './tasks/models/task-status';
-import { Response } from 'express';
 
 @Controller()
 @UseInterceptors(UserContextInterceptor)
 export class AppController {
   constructor(private readonly appService: AppService) {}
-
 }
