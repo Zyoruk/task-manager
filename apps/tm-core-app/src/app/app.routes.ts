5002f29fbe26c353c65f9ab60072b4b6048e9bbd
Luis Simon <lsimon@godaddy.com>
2024-09-16 17:19:25 -0600

    Core APP: Protect HomeComponent

--- apps/tm-core-app/src/app/app.routes.ts
+++ apps/tm-core-app/src/app/app.routes.ts
@@ -1,10 +1,17 @@
 import { Route } from '@angular/router';
 import { TmLoginComponent } from './tm-login/tm-login.component'; // Import the component
+import { AuthGuard } from './guards/auth.guard';
+import { HomeComponent } from './home/home.component';
 
 export const appRoutes: Route[] = [
   { 
     path: 'login', // The path in the URL (e.g., /login)
     component: TmLoginComponent 
+  },
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [AuthGuard], // Apply the guard here
   },
   // ... other routes
 ];
