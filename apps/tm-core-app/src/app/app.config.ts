905fbd3efb424fb1f7abd1338ae4b22ae6c9af5c
Luis Simon <lsimon@godaddy.com>
2024-09-16 17:06:29 -0600

    Core App: Hydrate token from localstorage

--- apps/tm-core-app/src/app/app.config.ts
+++ apps/tm-core-app/src/app/app.config.ts
@@ -6,13 +6,27 @@ import {
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { StoreModule } from '@ngrx/store';
+import {
+  ActionReducer,
+  ActionReducerMap,
+  MetaReducer,
+  StoreModule,
+} from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { authReducer } from './store/auth/auth.reducer';
 import { AuthEffects } from './store/auth/auth.effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from './environments/environment'; // Import your environment
+import { localStorageSync } from 'ngrx-store-localstorage';
 
+const reducers: ActionReducerMap<any> = { auth: authReducer };
+
+export function localStorageSyncReducer(
+  reducer: ActionReducer<any>
+): ActionReducer<any> {
+  return localStorageSync({ keys: ['auth'], rehydrate: true })(reducer);
+}
+const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -20,9 +34,11 @@ export const appConfig: ApplicationConfig = {
     provideRouter(appRoutes),
     provideHttpClient(),
     importProvidersFrom(
-      StoreModule.forRoot({ auth: authReducer }), // Register the auth reducer
+      StoreModule.forRoot(reducers, { metaReducers }), // Register the auth reducer
       EffectsModule.forRoot([AuthEffects]), // Register the effects
-      environment.production ? [] : StoreDevtoolsModule.instrument({ maxAge: 25 }),
+      environment.production
+        ? []
+        : StoreDevtoolsModule.instrument({ maxAge: 25 })
     ),
   ],
 };
