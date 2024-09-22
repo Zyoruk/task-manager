d01f8b072d6211d04ac01f4dd757a92d739f787a
Zyoruk <ce.zyoruk@gmail.com>
2024-09-21 20:56:52 -0600

    Core app: refactor to use new navigation items from shared libs

--- apps/tm-core-app/src/app/tm-core-app-layout/tm-core-app-layout.component.ts
+++ apps/tm-core-app/src/app/tm-core-app-layout/tm-core-app-layout.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { Router, RouterModule } from '@angular/router';
-import { Store } from '@ngrx/store';
 import { AuthFacade } from '@task-manager/shared/tm-store';
-
+import { TmHeaderComponent, TmSidenavComponent } from '@task-manager/tm-ui';
+import { Subject } from 'rxjs';
 @Component({
   selector: 'app-tm-core-app-layout',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, TmHeaderComponent, TmSidenavComponent],
   providers: [AuthFacade],
   templateUrl: './tm-core-app-layout.component.html',
   styleUrl: './tm-core-app-layout.component.css',
@@ -15,9 +15,13 @@ export class TmCoreAppLayoutComponent implements OnInit {
   @ViewChild('toastContainer', { read: ViewContainerRef, static: true })
   toastContainer!: ViewContainerRef;
   title = 'tm-core-app';
-
+  toggleDrawerSubject = new Subject<boolean>();
   constructor(private router: Router, private authFacade: AuthFacade) {}
 
+  handleMenuClick() {
+    console.log('handleMenuClick');
+    this.toggleDrawerSubject.next(true);
+  }
   logout() {
     this.router.navigate(['/login']); // Redirect to login after logout
     this.authFacade.logout();
